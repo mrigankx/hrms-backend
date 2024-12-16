85088ee79c469b701a57d3842d58a72110404114
Achal Singh <me1847940001@gmail.com>
2024-12-16 21:06:31 +0530

    larave project created

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Hello World', 'status'=>200]);
+});
